33ff6876585105acee568c5c47e5c8cc7dff12da
i.metlin <metlin.igor@gmail.com>
2018-10-02 16:17:02 +0300

    first

--- app/src/main/java/com/company/imetlin/fishmarker/MainActivity.java
+++ app/src/main/java/com/company/imetlin/fishmarker/MainActivity.java
@@ -2,6 +2,16 @@ package com.company.imetlin.fishmarker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.company.imetlin.fishmarker.adapters.AdapterGrid;
+import com.company.imetlin.fishmarker.pojo.ItemFirstActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        List<ItemFirstActivity> image_details = getListData();
+
+        final GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new AdapterGrid(this, image_details));
+
+        // When the user clicks on the GridItem
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+               // Object o = gridView.getItemAtPosition(position);
+               // ItemFirstActivity itemFirstActivity = (ItemFirstActivity) o;
+               // Toast.makeText(MainActivity.this, "Selected :"
+                   //     + " " + itemFirstActivity, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+    private  List<ItemFirstActivity> getListData() {
+
+        List<ItemFirstActivity> list = new ArrayList<ItemFirstActivity>();
+        ItemFirstActivity vietnam = new ItemFirstActivity("Ocean", "ocean");
+        ItemFirstActivity usa = new ItemFirstActivity("Lake", "ozero");
+        ItemFirstActivity russia = new ItemFirstActivity("River", "river");
+        ItemFirstActivity australia = new ItemFirstActivity("Sea", "sea");
+
+        list.add(vietnam);
+        list.add(usa);
+        list.add(russia);
+        list.add(australia);
+
+
+        return list;
+    }
+
 }
---
+++ app/src/main/java/com/company/imetlin/fishmarker/adapters/AdapterGrid.java
@@ -0,0 +1,81 @@
+package com.company.imetlin.fishmarker.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.company.imetlin.fishmarker.R;
+import com.company.imetlin.fishmarker.pojo.ItemFirstActivity;
+
+import java.util.List;
+
+public class AdapterGrid extends BaseAdapter {
+
+    private List<ItemFirstActivity> listData;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public AdapterGrid(Context aContext,  List<ItemFirstActivity> listData) {
+        this.context = aContext;
+        this.listData = listData;
+        layoutInflater = LayoutInflater.from(aContext);
+    }
+
+    @Override
+    public int getCount() {
+        return listData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.one_item_grid, null);
+            holder = new ViewHolder();
+            holder.PhotoWater = (ImageView) convertView.findViewById(R.id.imageViewWater);
+            holder.NameWater = (TextView) convertView.findViewById(R.id.textViewWaterName);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        ItemFirstActivity itemFirstActivity = this.listData.get(position);
+        holder.NameWater.setText(itemFirstActivity.getName());
+
+        int imageId = this.getMipmapResIdByName(itemFirstActivity.getPhoto());
+        holder.PhotoWater.setImageResource(imageId);
+
+        return convertView;
+    }
+
+    // Find Image ID corresponding to the name of the image (in the directory mipmap).
+    public int getMipmapResIdByName(String resName)  {
+        String pkgName = context.getPackageName();
+
+        // Return 0 if not found.
+        int resID = context.getResources().getIdentifier(resName , "drawable", pkgName);
+        Log.i("CustomGridView", "Res Name: "+ resName+"==> Res ID = "+ resID);
+        return resID;
+    }
+
+    static class ViewHolder {
+        ImageView PhotoWater;
+        TextView NameWater;
+
+    }
+
+}
