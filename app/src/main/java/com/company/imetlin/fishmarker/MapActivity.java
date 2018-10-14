7814f44478176a7a98826625e39447de9f8ed8e7
i.metlin <metlin.igor@gmail.com>
2018-10-14 12:09:26 +0300

    map

--- app/src/main/java/com/company/imetlin/fishmarker/MapActivity.java
+++ app/src/main/java/com/company/imetlin/fishmarker/MapActivity.java
@@ -1,5 +1,8 @@
 package com.company.imetlin.fishmarker;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +25,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     //GoogleMap map;
     final String TAG = "myLogs";
     GoogleMap googleMap;
+    private UiSettings mUiSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +36,32 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+
+        mUiSettings = googleMap.getUiSettings();
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mUiSettings.setZoomControlsEnabled(true);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+        mUiSettings.setMyLocationButtonEnabled(true);
 
     }
 }
