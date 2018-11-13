ea8f3801dbec7f5b5c3ecf75f3c11bee3834603c
i.metlin <metlin.igor@gmail.com>
2018-11-13 17:58:48 +0200

    devider

--- app/src/main/java/com/company/imetlin/fishmarker/WaterActivity.java
+++ app/src/main/java/com/company/imetlin/fishmarker/WaterActivity.java
@@ -3,7 +3,9 @@ package com.company.imetlin.fishmarker;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -17,12 +19,15 @@ import com.company.imetlin.fishmarker.pojo.ModelClass;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.security.AccessController.getContext;
+
 
 public class WaterActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
 
 
     @Override
@@ -46,13 +51,15 @@ public class WaterActivity extends AppCompatActivity {
         // создаем адаптер
         //mAdapter = new AdapterRecycler(image_details);
 
+
+        DividerItemDecoration divider = new DividerItemDecoration(mRecyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        divider.setDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.devider));
+        mRecyclerView.addItemDecoration(divider);
+
+
         mAdapter = new AdapterRecycler(image_details, new OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-
-
-
-
 
 
                 Intent intent = new Intent(WaterActivity.this, MapActivity.class);
