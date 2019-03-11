559f463c16d39787e5db8332235eae6b048c2bb2
i.metlin <metlin.igor@gmail.com>
2019-03-11 14:30:34 +0200

    reset

--- app/src/main/java/com/company/imetlin/fishmarker/Firebase/RegistrationActivity.java
+++ app/src/main/java/com/company/imetlin/fishmarker/Firebase/RegistrationActivity.java
@@ -54,7 +54,7 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
         mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.button_register).setOnClickListener(this);
-        ///////hgfh
+        ///////
 
 
         ArrayList<String> AllCountry = new ArrayList<String>();
