4bf85cf5b449e0b6848da98441bb90e893473e7b
i.metlin <metlin.igor@gmail.com>
2019-03-25 20:56:36 +0200

    bottom_sheet

--- app/src/main/java/com/company/imetlin/fishmarker/MapActivity.java
+++ app/src/main/java/com/company/imetlin/fishmarker/MapActivity.java
@@ -11,6 +11,7 @@ import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -109,6 +110,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mUiSettings.setMapToolbarEnabled(false);
 
 
+
         onCheckPermission(google);
 
         //mUiSettings.setMyLocationButtonEnabled(true);
@@ -133,8 +135,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             public boolean onMarkerClick(Marker marker) {
 
 
-                marker.showInfoWindow();;
+                marker.showInfoWindow();
                 NavigationBar navigationBar = new NavigationBar();
+
                 navigationBar.show(getSupportFragmentManager(),"exampleBottomSheet");
 
 
--- app/src/main/java/com/company/imetlin/fishmarker/Navigation/NavigationBar.java
+++ app/src/main/java/com/company/imetlin/fishmarker/Navigation/NavigationBar.java
@@ -2,12 +2,17 @@ package com.company.imetlin.fishmarker.Navigation;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,15 +23,31 @@ import com.company.imetlin.fishmarker.database.DatabaseLoad;
 public class NavigationBar extends BottomSheetDialogFragment {
 
     private BottomSheetListener mListener;
+    private BottomSheetBehavior bottomSheetBehavior;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ((View) getView().getParent()).setBackgroundColor(Color.TRANSPARENT);
+
+
+
+
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.bottom_sheet, container, false);
 
-       Button button_detail = v.findViewById(R.id.button_detail);
+
+
+        Button button_detail = v.findViewById(R.id.button_detail);
         Button button_edit = v.findViewById(R.id.button_edit);
-        button_detail.setOnClickListener(new View.OnClickListener() {
+
+       /* button_detail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mListener.onButtonClicked("Button 1 clicked");
@@ -39,10 +60,11 @@ public class NavigationBar extends BottomSheetDialogFragment {
                 mListener.onButtonClicked("Button 2 clicked");
                 dismiss();
             }
-        });
+        });*/
 
         return v;
     }
+
 
     public interface BottomSheetListener {
         void onButtonClicked(String text);
