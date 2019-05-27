c04eca03fcf0120d71d6113479b5dd547ff2d117
Igor755 <metlin.igor@gmail.com>
2019-05-27 18:40:51 +0300

    progress_activity

--- app/src/main/java/com/company/imetlin/fishmarker/dialog/ProgressActivity.java
+++ app/src/main/java/com/company/imetlin/fishmarker/dialog/ProgressActivity.java
@@ -1,11 +1,16 @@
 package com.company.imetlin.fishmarker.dialog;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.company.imetlin.fishmarker.MainActivity;
 import com.company.imetlin.fishmarker.R;
@@ -16,9 +21,10 @@ import me.itangqi.waveloadingview.WaveLoadingView;
 public class ProgressActivity extends AppCompatActivity {
 
     public WaveLoadingView waveLoadingView;
-    public SeekBar seekBar;
     private int progress = 0;
     private Handler handler = new Handler();
+    TextView textView;
+    Thread thread;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,37 +32,46 @@ public class ProgressActivity extends AppCompatActivity {
         setContentView(R.layout.progress_activity);
 
         waveLoadingView = (WaveLoadingView) findViewById(R.id.waveLoading);
-        waveLoadingView.setProgressValue(0);
+        waveLoadingView.setWaterLevelRatio(0.0f);
+        waveLoadingView.setProgressValue(progress);
+        textView = (TextView) findViewById(R.id.textview);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        textView.startAnimation(anim);
+        Typeface tf = Typeface.createFromAsset(getAssets(),
+                "font_progress_activity.otf");
+        textView.setTypeface(tf);
 
 
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (progress < 100){
-                    progress++;
-                    android.os.SystemClock.sleep(100);
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            waveLoadingView.setProgressValue(progress);
 
-                        }
-                    });
+         thread  =   new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (progress < 100) {
+                        progress++;
+                        android.os.SystemClock.sleep(100);
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                waveLoadingView.setProgressValue(progress);
 
+                                if(progress == 100){
+                                    thread.interrupt();
+                                    finish();
+                                    Intent intent = new Intent(ProgressActivity.this, SignInActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+                    }
                 }
+            });thread.start();
 
-            }
-        }).start();
+        }
 
-      /*  Intent intent = new Intent(ProgressActivity.this, SignInActivity.class);
-        startActivity(intent);
-*/
 
     }
 
 
 
 
-
-}
